a88f463664be41de9f22b7e0c152b4d7022aa1d3
mr.Zhuravsky <sashazhuravsky9698@gmail.com>
2025-12-02 11:11:43 +0100

    "first commit"

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,13 @@
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//   a * b}
+
+let a 
+let b
+
+function multiply(a, b){
+ return a * b
+}
+c = multiply(2,3)
+console.log(c)
