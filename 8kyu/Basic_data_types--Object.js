1ca32d5e4e55bf3fe9d816e563a237de23858dfe
mr.Zhuravsky <sashazhuravsky9698@gmail.com>
2025-12-16 12:19:02 +0100

    add: implement animal function to describe an animal object

---
+++ 8kyu/Basic_data_types--Object.js
@@ -0,0 +1,30 @@
+// In JavaScript, Object is one of basic data types. To define an object you can use let obj = new Object() or let obj = {}.
+
+// You can define the object attributes during initialization, like this:
+
+// let animal = {name: "dog"}
+// you can also set/get some properties after the object definition, like this:
+
+// let animal = {}
+// animal.name = "dog"
+// // or:
+// animal["name"] = "dog"
+// Task
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+let animal ={
+    name: "dog",
+    legs: 4,
+    color: "white"
+}
+
+function returnAnimal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+}
+
+console.log(returnAnimal(animal))
